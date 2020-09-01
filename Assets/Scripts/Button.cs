a2482485a602ca7f228d301b72cb3f099f1d8161
strfx1 <strfx.1@gmail.com>
2020-09-01 14:34:49 -0700

    Create Button.cs

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    public event EventHandler OnButtonClicked;
+
+    public void ProcessButtonClicked()
+    {
+        var handler = OnButtonClicked;
+        if(handler != null)
+        {
+            handler(this, EventArgs.Empty);
+        }
+    }
+}
