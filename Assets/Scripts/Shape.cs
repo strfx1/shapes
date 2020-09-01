2491d5448e83079add8b4e1e02d2f2f96b9d9f2f
strfx1 <strfx.1@gmail.com>
2020-09-01 14:36:06 -0700

    Create Shape.cs

---
+++ Assets/Scripts/Shape.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shape : MonoBehaviour
+{
+    float timerForDoubleClick = 0.0f;
+    float delay = 0.3f;
+    bool isDoubleClick = false;
+
+    public event EventHandler OnDoubleClick;
+
+    void Update()
+    {
+        if (isDoubleClick == true)
+        {
+            timerForDoubleClick += Time.deltaTime;
+        }
+
+
+        if (timerForDoubleClick >= delay)
+        {
+            timerForDoubleClick = 0.0f;
+            isDoubleClick = false;
+        }
+
+    }
+
+    void OnMouseOver()
+    {
+        if (Input.GetButtonDown("Fire1") && isDoubleClick == false)
+        {
+            // Debug.Log("Mouse clicked once");
+            isDoubleClick = true;
+        }
+    }
+
+    void OnMouseDown()
+    {
+        if (isDoubleClick == true && timerForDoubleClick < delay)
+        {
+            // Debug.Log("ISWORKING!!!!");
+
+            // Notify others of double click.
+            var handler = OnDoubleClick;
+            if(handler != null)
+            {
+                handler(this, EventArgs.Empty);
+            }
+        }
+    }
+}
